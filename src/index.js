795bf923144d89f9ad9b051a9edde7f600f57a7d
mohehexd <dumblemoe@gmail.com>
2020-12-12 21:06:25 -0600

    Adding event listener for high score

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("hello")
+const nameForm = document.getElementById("name-form")
+const nameInput = document.getElementById("name-input")
+const playerName = document.getElementById("player-name")
+
+nameForm.addEventListener("submit", submitName)
+
+function submitName(e){
+    e.preventDefault()
+
+    const ol = document.createElement('ol')
+    const li = document.createElement('li')
+    li.innerText = nameInput.value + " - "
+
+    ol.append(li)
+
+    playerName.appendChild(ol)
+}
