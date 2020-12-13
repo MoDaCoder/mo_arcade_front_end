4928334e2dde3c8103f55973572ef8f38bd3ad1d
mohehexd <dumblemoe@gmail.com>
2020-12-12 21:37:15 -0600

    Changing event listener's name for player name to renderName

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ const nameForm = document.getElementById("name-form")
 const nameInput = document.getElementById("name-input")
 const playerName = document.getElementById("player-name")
 
-nameForm.addEventListener("submit", submitName)
+nameForm.addEventListener("submit", renderName)
 
-function submitName(e){
-    e.preventDefault()
+function submitName(){
+
+}
+
+//render player name to DOM
+function renderName(){
+    event.preventDefault()
 
     const ol = document.createElement('ol')
     const li = document.createElement('li')
-    li.innerText = nameInput.value + " - "
+    li.innerText = nameInput.value + " -"
 
     ol.append(li)
 
     playerName.appendChild(ol)
+
+    nameForm.reset()
 }
