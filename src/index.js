5f1d7d71759319f0930d9d0bcff2260864dbeb14
mohehexd <dumblemoe@gmail.com>
2020-12-18 17:14:01 -0600

    Creating a fetch function for players name.

--- src/index.js
+++ src/index.js
@@ -3,31 +3,47 @@
 const scoreForm = document.getElementById("score-form")
 const scoreInput = document.getElementById("score-input")
 const scoreUl = document.getElementById('score-ul')
-const boards = 'http://localhost:3000/boards'
+const boardsURL = 'http://localhost:3000/boards'
+//Player Name
+const nameForm = document.getElementById("name-form")
+// const nameInput = document.getElementById("name-input")
+const ol = document.getElementById('ol1')
+const playersURL = 'http://localhost:3000/players'
 
-//Post Score Fetch
-console.log('about to post a score');
-
-fetch(boards).then(response => {
-    console.log(response);
-    return response.boards();
-})
-.then(response => { 
-    console.log(response);
-});
-
-function submitScore(){
-
-
+//Get Boards Score Fetch
+function fetchBoards(){
+    fetch(boardsURL)
+    .then(res => res.json())
+    .then(boards => boards.forEach(board => renderScore(board.score)))
 }
 
-scoreForm.addEventListener("submit", renderScore)
+scoreForm.addEventListener("submit", submitScore)
 
-function renderScore(){
+//Post Boards Score Fetch
+console.log('about to post a score');
+function submitScore(){
     event.preventDefault()
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body:JSON.stringify({
+            score: parseInt(scoreInput.value)
+        })
+    }
+    fetch(boardsURL, configObj)
+
+    renderScore(scoreInput.value)
+}
+
+function renderScore(score){
+    console.log(score)
+    // event.preventDefault()
 
     const li = document.createElement('li')
-    li.innerText = scoreInput.value
+    li.innerText = score
 
     scoreUl.append(li)
 
@@ -35,26 +51,38 @@ function renderScore(){
 }
 
 //Player Name
-const nameForm = document.getElementById("name-form")
-const nameInput = document.getElementById("name-input")
-const ol = document.getElementById('ol1')
 
 nameForm.addEventListener("submit", renderName)
 
-function submitName(){
-
-}
 // **** CHANGE FUNCTION TO INPUT MOCK SCORE FIIIIRRRSSTTT ****
 // **** OTHERWISE FETCH POST TO WON'T WORK IF NAME IS FIRST ****
 //render player name to DOM
-function renderName(){
-    event.preventDefault()
-
+function renderName(e){
+    e.preventDefault()
+    const nameInput = e.target.children[0].value
     const ol = document.getElementById('ol1')
     const li = document.createElement('li')
-    li.innerText = nameInput.value + " -"
 
+    li.innerText = nameInput + " -"
     ol.append(li)
 
-    nameForm.reset()
+    submitName(nameInput)
+
+    e.target.reset()
 }
+
+function submitName(name){
+    fetch(playersURL, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            player: name
+        })
+    })
+
+}
+
+fetchBoards()
