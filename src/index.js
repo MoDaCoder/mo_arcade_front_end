cf1fc559c972009a13649388ee56cd4e65b1dd81
mohehexd <dumblemoe@gmail.com>
2020-12-25 14:45:01 -0600

    Updating POST fetch to accpet board_id with players name and creating urban flipporama

--- src/index.js
+++ src/index.js
@@ -54,8 +54,6 @@ function renderScore(score){
 
 nameForm.addEventListener("submit", renderName)
 
-// **** CHANGE FUNCTION TO INPUT MOCK SCORE FIIIIRRRSSTTT ****
-// **** OTHERWISE FETCH POST TO WON'T WORK IF NAME IS FIRST ****
 //render player name to DOM
 function renderName(e){
     e.preventDefault()
@@ -72,6 +70,7 @@ function renderName(e){
 }
 
 function submitName(name){
+    // debugger
     fetch(playersURL, {
         method: "POST",
         headers: {
@@ -79,10 +78,158 @@ function submitName(name){
             "Accept": "application/json"
         },
         body: JSON.stringify({
-            player: name
+            player: {name: name, board_id: current_board }
         })
     })
 
 }
 
 fetchBoards()
+
+// ****Urban Flipporama****
+document.addEventListener('DOMContentLoaded', () => {
+
+    //card options-
+    const cardArray = [
+        {
+            name: 'borb',
+            img: 'images/borb.png'
+        },
+        {
+            name: 'borb',
+            img: 'images/borb.png'
+        },
+        {
+            name: 'fishbowl',
+            img: 'images/fishbowl.png'
+        },
+        {
+            name: 'fishbowl',
+            img: 'images/fishbowl.png'
+        },
+        {
+            name: 'food',
+            img: 'images/food.png'
+        },
+        {
+            name: 'food',
+            img: 'images/food.png'
+        },        
+        {
+            name: 'french_hat',
+            img: 'images/french_hat.png'
+        },
+        {
+            name: 'french_hat',
+            img: 'images/french_hat.png'
+        },
+        {
+            name: 'garfield',
+            img: 'images/garfield.png'
+        },
+        {
+            name: 'garfield',
+            img: 'images/garfield.png'
+        },
+        {
+            name: 'llama',
+            img: 'images/llama.png'
+        },        
+        {
+            name: 'llama',
+            img: 'images/llama.png'
+        },
+        {
+            name: 'panda',
+            img: 'images/panda.png'
+        },
+        {
+            name: 'panda',
+            img: 'images/panda.png'
+        },
+        {
+            name: 'snail',
+            img: 'images/snail.png'
+        },        
+        {
+            name: 'snail',
+            img: 'images/snail.png'
+        },
+        {
+            name: 'richard',
+            img: 'images/richard.png'
+        },
+        {
+            name: 'richard',
+            img: 'images/richard.png'
+        },
+        {
+            name: 'mom',
+            img: 'images/mom.png'
+        },
+        {
+            name: 'mom',
+            img: 'images/mom.png'
+        }
+    ]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+//You Used let here instead of var/Might cause errors
+const grid = document.querySelector('.grid')
+const resultDisplay = document.querySelector('#result')
+let cardsChosen = []
+let cardsChosenId = []
+let cardsWon = []
+
+//creating board
+//using for loop to iterate through card array and adding an img element to it
+function createBoard() {
+  for (let i = 0; i < cardArray.length; i++) {
+      let card = document.createElement('img')
+      card.setAttribute('src', 'images/pikachu.png')
+      card.setAttribute('data-id', i)
+      card.addEventListener('click', flipCard)
+    grid.appendChild(card)
+  }
+}
+
+// check for matches
+function checkForMatch() {
+    let cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+
+    if (cardsChosen[0] === cardsChosen[1]) {
+        alert('You found a match!')
+        cards[optionOneId].setAttribute('src', 'images/coding.png')
+        cards[optionTwoId].setAttribute('src', 'images/coding.png')
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/pikachu.png')
+        cards[optionTwoId].setAttribute('src', 'images/pikachu.png')
+        alert("Nope that's the wrong numba!")
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if (cardsWon.length === cardArray.length/2) {
+        resultDisplay.textContent = 'Congratulations! You won!'
+    }
+}
+
+// flip your card
+// You Used let here instead of let/Might cause errors
+function flipCard() {
+    let cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500)
+    }
+}
+
+createBoard()
+
+})
