468ca474f36d699273cfe46bc67f547b3a3b7011
mohehexd <dumblemoe@gmail.com>
2021-01-11 15:40:15 -0600

    Creating score renderScore function and fetch Post function.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,51 @@
 //**** Score Board ****
 //Board Name
-const boardNameForm = document.getElementById("board-name-form")
-const boardPlayer = document.getElementById("board-player")
-const boardNameInput = document.getElementById("board-name-Input")
-const boardTitle = document.getElementById("h3")
+const boardNameForm = document.getElementById('board-name-form')
+const boardPlayer = document.getElementById('board-player')
+const boardNameInput = document.getElementById('board-name-Input')
+const boardTitle = document.getElementById('h3')
 const boardsURL = 'http://localhost:3000/boards'
 //Player Name & Score
 const scoreTitle = document.getElementById('score-title')
 // scoreTitle.style.display = "none"
-
 
 const playersURL = 'http://localhost:3000/players'
 const playerForm = document.getElementById('player-form')
 const playerInput = document.getElementById('player-input')
 const playerName = document.getElementById('player-name')
 
+const scoreURL = 'http://localhost:3000/scores'
+const scoreForm = document.getElementById('score-form')
+const scoreInput = document.getElementById('score-input')
+const scoreNumber = document.getElementById('player-score')
+
 playerForm.addEventListener("submit", Player.submitPlayer)
 playerForm.addEventListener("submit", Board.fetchBoards)
+scoreForm.addEventListener("submit", renderScore)
 
+function renderScore(){
+    event.preventDefault()
+    const scoreLi = document.createElement('li')
+    scoreLi.innerText = scoreInput.value
+    scoreNumber.append(scoreLi)
+    submitScore(scoreInput.value)
+    scoreForm.reset()
+}
+
+function submitScore(score){
+    console.log(score)
+    fetch(scoreURL, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            score_value: score,
+            player_id: 1
+        })
+    })
+}
 
 Board.fetchBoards()
 Player.fetchPlayers()
--- src/player.js
+++ src/player.js
@@ -52,8 +52,6 @@ class Player {
                 }
             })
         }
-
-        // debugger
         fetch(playersURL, configObj)
         .then(res => res.json())
         .then(data => {
@@ -62,63 +60,6 @@ class Player {
             newPlayers.renderPlayer()
         })
     }
-
-
-
-
-    // static fetchPlayers(){
-    //     fetch(playersURL)
-    //     .then(res => res.json())
-    //     .then(players => {
-    //         console.log(players)
-    //         for (let player of players.data){
-    //             let newPlayer = new Player(player.data)
-    //         }
-    //         Player.renderPlayer()
-    //     })
-    // }
-
-    // fetchPlayers(){
-    //     fetch(playersURL)
-    //     .then(res => res.json())
-    //     .then(console.log)
-    // }
-
-    // static renderPlayer(){
-    //     const playerLi = document.createElement('li')
-    //     playerLi.innerText = playerInput.value
-    //     playerName.appendChild(playerLi)
-    //     playerForm.reset()
-    // }
-
-//     static renderPlayer(event){
-//         console.log(event)
-//         const playerLi = document.createElement('li')
-//         playerLi.innerText = this.name
-//         console.log(playerLi)
-//         playerName.appendChild(playerLi)
-//         playerForm.reset()
-//     }
-
-//     //render player name to DOM
-//     //Post Player
-//     static submitPlayer(nameInput) {
-//         // const nameInput = event
-//         // console.log(event.target.innerHTML)
-//         const configObj = {
-//             method: "POST",
-//             headers: {
-//                 "Content-type": "application/json",
-//                 "Accept": "application/json"
-//             },
-//             body: JSON.stringify({
-//                 player: {
-//                     name: nameInput
-//                 }
-//             })
-//         }
-//         fetch(playersURL, configObj)
-//     }
 }
 
 
